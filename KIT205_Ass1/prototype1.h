1db98de3555fce46ca2e85064b5ffa6bec64d28f
Your Name <your-email@example.com>
2026-05-13 23:08:55 +1000

    prototype1.h complete

--- KIT205_Ass1/prototype1.h
+++ KIT205_Ass1/prototype1.h
@@ -1,1 +1,22 @@
 #pragma once
+
+
+#include "product_bst.h"
+
+typedef struct CustomerNode {
+    int customerID;
+    ProductNode* products;
+    struct CustomerNode* next;
+} CustomerNode;
+
+CustomerNode* createCustomerNode(int customerID);
+CustomerNode* findCustomer(CustomerNode* head, int customerID);
+CustomerNode* insertCustomer(CustomerNode* head, int customerID);
+CustomerNode* addPurchasePrototype1(CustomerNode* head, int customerID, int productID);
+
+void printCustomersPrototype1(CustomerNode* head);
+void printProductsForCustomerPrototype1(CustomerNode* head, int customerID);
+void printCustomersForProductPrototype1(CustomerNode* head, int productID);
+
+void freeCustomersPrototype1(CustomerNode* head);
+void testPrototype1();
