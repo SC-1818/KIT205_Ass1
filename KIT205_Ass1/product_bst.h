27f623b9e64a9afcf370e948c65d3fb8af718bf3
Your Name <your-email@example.com>
2026-05-13 22:38:12 +1000

    Product_bst.h

--- KIT205_Ass1/product_bst.h
+++ KIT205_Ass1/product_bst.h
@@ -1,1 +1,13 @@
 #pragma once
+
+typedef struct ProductNode {
+    int productID;
+    struct ProductNode* left;
+    struct ProductNode* right;
+} ProductNode;
+
+ProductNode* createProductNode(int productID);
+ProductNode* insertProduct(ProductNode* root, int productID);
+int findProduct(ProductNode* root, int productID);
+void printProducts(ProductNode* root);
+void freeProducts(ProductNode* root);
