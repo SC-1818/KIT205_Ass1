b23e6aad9d57808855301b7d1e2861758b6578e8
Your Name <your-email@example.com>
2026-05-13 22:51:58 +1000

    product_bst.c complete

--- KIT205_Ass1/product_bst.c
+++ KIT205_Ass1/product_bst.c
@@ -31,3 +31,35 @@ ProductNode* insertProduct(ProductNode* root, int productID) {
 
     return root;
 }
+
+int findProduct(ProductNode* root, int productID) {
+    if (root == NULL) {
+        return 0;
+    }
+
+    if (productID == root->productID) {
+        return 1;
+    }
+    else if (productID < root->productID) {
+        return findProduct(root->left, productID);
+    }
+    else {
+        return findProduct(root->right, productID);
+    }
+}
+
+void printProducts(ProductNode* root) {
+    if (root != NULL) {
+        printProducts(root->left);
+        printf("%d ", root->productID);
+        printProducts(root->right);
+    }
+}
+
+void freeProducts(ProductNode* root) {
+    if (root != NULL) {
+        freeProducts(root->left);
+        freeProducts(root->right);
+        free(root);
+    }
+}
