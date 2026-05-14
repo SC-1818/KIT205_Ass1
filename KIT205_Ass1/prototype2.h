7cdd93344f1e223a9cb90089750e531a4521cbe4
Your Name <your-email@example.com>
2026-05-14 23:29:55 +1000

    Adding prototype2.h and hash table sudtomer structure

--- KIT205_Ass1/prototype2.h
+++ KIT205_Ass1/prototype2.h
@@ -1,1 +1,26 @@
 #pragma once
+#include "product_bst.h"
+
+#define TABLE_SIZE 10007
+
+typedef struct HashCustomerNode {
+    int customerID;
+    ProductNode* products;
+    struct HashCustomerNode* next;
+} HashCustomerNode;
+
+int hashFunction(int customerID);
+void setupHashTable(HashCustomerNode* table[]);
+
+HashCustomerNode* createHashCustomerNode(int customerID);
+HashCustomerNode* findHashCustomer(HashCustomerNode* table[], int customerID);
+void insertHashCustomer(HashCustomerNode* table[], int customerID);
+
+void addPurchasePrototype2(HashCustomerNode* table[], int customerID, int productID);
+
+void printCustomersPrototype2(HashCustomerNode* table[]);
+void printProductsForCustomerPrototype2(HashCustomerNode* table[], int customerID);
+void printCustomersForProductPrototype2(HashCustomerNode* table[], int productID);
+
+void freeHashTable(HashCustomerNode* table[]);
+void testPrototype2();
