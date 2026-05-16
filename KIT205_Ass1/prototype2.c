d4ea6c6329ab9f4c888686f393ab108320a1c567
Your Name <your-email@example.com>
2026-05-16 23:14:28 +1000

    hash customer create, search, and insert

--- KIT205_Ass1/prototype2.c
+++ KIT205_Ass1/prototype2.c
@@ -15,3 +15,48 @@ void setupHashTable(HashCustomerNode* table[]) {
         table[i] = NULL;
     }
 }
+
+HashCustomerNode* createHashCustomerNode(int customerID) {
+    HashCustomerNode* newNode = (HashCustomerNode*)malloc(sizeof(HashCustomerNode));
+
+    if (newNode == NULL) {
+        printf("Memory allocation failed.\n");
+        return NULL;
+    }
+
+    newNode->customerID = customerID;
+    newNode->products = NULL;
+    newNode->next = NULL;
+
+    return newNode;
+}
+
+HashCustomerNode* findHashCustomer(HashCustomerNode* table[], int customerID) {
+    int index = hashFunction(customerID);
+    HashCustomerNode* current = table[index];
+
+    while (current != NULL) {
+        if (current->customerID == customerID) {
+            return current;
+        }
+
+        current = current->next;
+    }
+
+    return NULL;
+}
+
+void insertHashCustomer(HashCustomerNode* table[], int customerID) {
+    int index;
+    HashCustomerNode* newNode;
+
+    if (findHashCustomer(table, customerID) != NULL) {
+        return;
+    }
+
+    index = hashFunction(customerID);
+    newNode = createHashCustomerNode(customerID);
+
+    newNode->next = table[index];
+    table[index] = newNode;
+}
